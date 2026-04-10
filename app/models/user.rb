536ddf5f9b6fba0cf5c9b9e7eaed6f36cbe39c62
JoaoVicttor07 <joaovictorsdc07@gmail.com>
2026-04-10 00:08:38 -0300

    fix: set resident as default role in User model enum

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  enum :role, { resident: 0, collaborator: 1, administrator: 2 }
+  enum :role, { resident: 0, collaborator: 1, administrator: 2 }, default: :resident
   validates :name, presence: true
 end
