679594869e86111e9522f7e05fef5a803b64d18d
JoaoVicttor07 <joaovictorsdc07@gmail.com>
2026-04-13 21:54:36 -0300

    feat(auth): disable self-registration and password recovery

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,7 @@ class User < ApplicationRecord
   has_many :user_ticket_types, dependent: :destroy
   has_many :assigned_ticket_types, through: :user_ticket_types, source: :ticket_type
 
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :rememberable, :validatable
   enum :role, { resident: 0, collaborator: 1, administrator: 2 }, default: :resident
   validates :name, presence: true
 end
