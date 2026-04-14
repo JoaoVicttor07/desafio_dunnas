9c8754d3999017fc52617322146e039a9d19fea6
JoaoVicttor07 <joaovictorsdc07@gmail.com>
2026-04-14 01:23:52 -0300

    feat(seed): bootstrap administrator user on first setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,3 +14,16 @@ TicketStatus.find_or_create_by!(name: "Concluído") do |s|
 end
 
 TicketStatus.where.not(id: open.id).update_all(is_default: false)
+
+if User.where(role: :administrator).none?
+  admin_email = ENV.fetch("ADMIN_EMAIL", "admin@admin.com")
+  admin_password = ENV.fetch("ADMIN_PASSWORD", "123456")
+
+  User.create!(
+    name: "Administrador",
+    email: admin_email,
+    role: :administrator,
+    password: admin_password,
+    password_confirmation: admin_password
+  )
+end
