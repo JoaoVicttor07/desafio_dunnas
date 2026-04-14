b969856ea0bf657b424db8bae1b0c77da49fed61
JoaoVicttor07 <joaovictorsdc07@gmail.com>
2026-04-14 00:30:17 -0300

    refactor(blocks): standardize controller to HTML-only flow

--- app/controllers/blocks_controller.rb
+++ app/controllers/blocks_controller.rb
@@ -1,15 +1,11 @@
 class BlocksController < ApplicationController
   before_action :authenticate_user!
   load_and_authorize_resource
-  before_action :set_block, only: %i[ show edit update destroy ]
+  before_action :set_block, only: %i[edit update destroy]
 
-  # GET /blocks or /blocks.json
+  # GET /blocks
   def index
     @blocks = Block.all
-  end
-
-  # GET /blocks/1 or /blocks/1.json
-  def show
   end
 
   # GET /blocks/new
@@ -21,52 +17,39 @@ class BlocksController < ApplicationController
   def edit
   end
 
-  # POST /blocks or /blocks.json
+  # POST /blocks
   def create
     @block = Block.new(block_params)
 
-    respond_to do |format|
-      if @block.save
-        format.html { redirect_to @block, notice: "Bloco criado com sucesso." }
-        format.json { render :show, status: :created, location: @block }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @block.errors, status: :unprocessable_entity }
-      end
+    if @block.save
+      redirect_to blocks_path, notice: "Bloco criado com sucesso."
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /blocks/1 or /blocks/1.json
+  # PATCH/PUT /blocks/1
   def update
-    respond_to do |format|
-      if @block.update(block_params)
-        format.html { redirect_to @block, notice: "Bloco atualizado com sucesso.", status: :see_other }
-        format.json { render :show, status: :ok, location: @block }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @block.errors, status: :unprocessable_entity }
-      end
+    if @block.update(block_params)
+      redirect_to blocks_path, notice: "Bloco atualizado com sucesso.", status: :see_other
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
-  # DELETE /blocks/1 or /blocks/1.json
+  # DELETE /blocks/1
   def destroy
     @block.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to blocks_path, notice: "Bloco removido com sucesso.", status: :see_other }
-      format.json { head :no_content }
-    end
+    redirect_to blocks_path, notice: "Bloco removido com sucesso.", status: :see_other
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_block
-      @block = Block.accessible_by(current_ability).find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def block_params
-      params.require(:block).permit(:identification, :floors_count, :apartments_per_floor)
-    end
+  def set_block
+    @block = Block.accessible_by(current_ability).find(params[:id])
+  end
+
+  def block_params
+    params.require(:block).permit(:identification, :floors_count, :apartments_per_floor)
+  end
 end
