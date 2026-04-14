9e3ee4695402a57bb47e2064de1baace23ead04e
JoaoVicttor07 <joaovictorsdc07@gmail.com>
2026-04-14 13:45:46 -0300

    fix(admin): disallow role changes for current user

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -46,7 +46,7 @@ module Admin
     private
 
     def user_params
-      permitted = [:name, :email, :role, :password, :password_confirmation]
+      permitted = [:name, :email, :password, :password_confirmation]
       permitted << :role unless @user == current_user
       p = params.require(:user).permit(*permitted)
 
