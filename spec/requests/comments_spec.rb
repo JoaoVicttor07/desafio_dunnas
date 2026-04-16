f722f6f0aba355d91a5b2dcb7920bf54e2fb4afb
JoaoVicttor07 <joaovictorsdc07@gmail.com>
2026-04-15 22:38:12 -0300

    test(comments): add request coverage for photo uploads Add request spec for comment creation with attached image. Use uploaded file payload to validate attachment persistence in the created comment. Assert redirect behavior remains correct after successful comment submission.

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -1,4 +1,5 @@
 require "rails_helper"
+require "base64"
 
 RSpec.describe "Comments", type: :request do
   describe "POST /tickets/:ticket_id/comments" do
@@ -49,6 +50,38 @@ RSpec.describe "Comments", type: :request do
       end.to change(Comment, :count).by(1)
 
       expect(response).to redirect_to(ticket_path(ticket))
+    end
+
+    it "allows resident to attach photos when creating a comment" do
+      resident = create(:user, :resident)
+      unit = create(:unit)
+      create(:user_unit, user: resident, unit: unit)
+      ticket = create(:ticket, user: resident, unit: unit)
+
+      sign_in resident
+
+      image_file = Tempfile.new(["comment-photo", ".png"])
+      image_file.binmode
+      image_file.write(
+        Base64.decode64("iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8Xw8AAuMB9oNbyzQAAAAASUVORK5CYII=")
+      )
+      image_file.rewind
+
+      uploaded_file = Rack::Test::UploadedFile.new(image_file.path, "image/png")
+
+      expect do
+        post ticket_comments_path(ticket), params: {
+          comment: {
+            body: "Comentario com foto",
+            photos: [uploaded_file]
+          }
+        }
+      end.to change(Comment, :count).by(1)
+
+      expect(Comment.last.photos).to be_attached
+      expect(response).to redirect_to(ticket_path(ticket))
+    ensure
+      image_file.close!
     end
   end
 end
